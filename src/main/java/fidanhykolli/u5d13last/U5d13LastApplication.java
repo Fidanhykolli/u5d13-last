6f9145dde00c573524647ed6671212426216fe9b
Fidan Hykolli <fidan-323@live.com>
2024-04-19 11:47:27 +0200

    Try Commit Main
    Provo commit Main

---
+++ src/main/java/fidanhykolli/u5d13last/U5d13LastApplication.java
@@ -0,0 +1,13 @@
+package fidanhykolli.u5d13last;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5d13LastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5d13LastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fidanhykolli/u5d13last/U5d13LastApplicationTests.java
@@ -0,0 +1,13 @@
+package fidanhykolli.u5d13last;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5d13LastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
